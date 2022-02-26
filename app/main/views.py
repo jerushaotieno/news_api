f699cbe5687994e2ae29fb68fc8cea5f15ca45a1
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-26 17:06:27 +0300

    fix function to render html templates correctly

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,17 @@
-from app import app
 from flask import render_template
+from app import app
 
 @app.route('/')
 def index():
-    return "News2 Homepage"
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    # return "News2 Homepage"
     return render_template("index.html")
 
 @app.route('/sources')
 def sources():
-    return "Source News Articles"
+    # return "Source News Articles"
     return render_template("sources.html")
