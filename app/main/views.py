8a6f235f1ae96d1a6ee84b58a39d522a9ac16cc8
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-26 16:35:40 +0300

    render template files in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
     return "News2 Homepage"
+    return render_template("index.html")
 
 @app.route('/sources')
 def sources():
     return "Source News Articles"
+    return render_template("sources.html")
