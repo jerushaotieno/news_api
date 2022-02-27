5fc162491381f2b8f31d0bf71d6ec380bd44b7c0
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-27 21:23:02 +0300

    add functions for user to view all articles fron one source

--- app/models.py
+++ app/models.py
@@ -12,11 +12,12 @@ class Sources:
 class Articles:
     '''
     '''
-    def __init__(self, article_title, article_author, article_url, article_url_to_image, article_published_at):
+    def __init__(self, article_title, article_author, article_description, article_url, article_url_to_image, article_published_at):
         '''
         '''
         self.article_title = article_title
         self.article_author = article_author
+        self.article_description = article_description
         self.article_url = article_url
         self.article_url_to_image = article_url_to_image
         self.article_published_at = article_published_at
--- app/requests.py
+++ app/requests.py
@@ -1,8 +1,10 @@
 from app import app
-from .models import Sources, Articles, Headlines
+from .models import Sources, Articles
+import urllib.request, json
 from newsapi import NewsApiClient
 
 key = app.config['API_KEY']
+url = app.config['SOURCE_URL']
 newsapi = NewsApiClient(api_key = key)
 
 def sources():
@@ -13,7 +15,41 @@ def sources():
     data_list = data['sources']
     source_list = []
     for item in data_list:
-        new_source = Sources(item['name'], item['description'], item['url'])
+        new_source = Sources(item['id'], ['name'], item['description'], item['url'])
         source_list.append(new_source)
 
     return source_list
+
+def articles(source_id):
+    '''
+    function to get all English news sources in a list
+    '''
+    source_url = url.format(source_id, key)
+    with urllib.request.urlopen(source_url) as uri:
+        result = uri.read()
+        response = json.loads(result)
+
+        article_results = []
+
+        if response['articles']:
+            source_data_list = response['articles']
+            article_results = get_data(source_data_list)
+    return article_results
+
+
+def get_data(source_dict):
+    '''
+    '''
+    article_list = []
+    for item in source_dict:
+        article_title = item.get('article_title')
+        article_author = item.get('article_author')
+        article_description = item.get('article_description')
+        article_url = item.get('article_url')
+        article_url_to_image = item.get('article_url_to_image')
+        article_published_at = item.get('article_published_at')
+
+        if article_url_to_image and article_url:
+            new_article = Articles(article_title, article_author, article_description, article_url, article_url_to_image, article_published_at)
+            article_list.append(new_article)     
+    return article_list 
