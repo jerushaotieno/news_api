1b4a41c27bdd50da99f218a9cfb101a00320fc78
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-27 12:51:23 +0300

    create a class for sources and articles in models.py

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+    '''
+    '''
+    def __init__(self, sourceName, sourceDescription, sourceUrl):
+        '''
+        '''
+        self.sourceName = sourceName
+        self.sourceDescription = sourceDescription
+        self.sourceUrl = sourceUrl
+
+class Articles:
+    '''
+    '''
