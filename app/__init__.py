e2516dc980a9911fa887a2d96c5eda7352053b6c
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-01 06:51:23 +0300

    update __init__.py to create application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
-from config import DevelopmentConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(DevelopmentConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
-from app import views
-from app import errors
+def create_app(config_name):
+    
+    app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
